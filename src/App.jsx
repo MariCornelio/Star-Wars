3f119693f36870685e7fc6d28284c4bf680d2981
Mari Cornelio <katherinacornelio@gmail.com>
2023-08-20 14:09:41 -0500

    fixing routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import { SectionHeader } from './components/atoms/SectionHeader';
 
+import { useContext } from 'react';
+import { CharacterContext } from './components/context/CharacterContext';
+
 const App = () => {
   const isSmallScreen = window.innerWidth <= 550;
+  const { character } = useContext(CharacterContext);
   return (
     <Router>
       <Routes>
@@ -25,7 +29,7 @@ const App = () => {
         ) : (
           <Route exact path="/" element={<Home />} />
         )}
-        <Route
+        {/* <Route
           path="/details"
           element={
             <>
@@ -33,7 +37,27 @@ const App = () => {
               <DetailsCharacter />
             </>
           }
-        />
+        /> */}
+        {Object.keys(character).length === 0 ? (
+          <Route
+            path="/details"
+            element={
+              <>
+                <SectionHeader /> <CharacterList />
+              </>
+            }
+          />
+        ) : (
+          <Route
+            path="/details"
+            element={
+              <>
+                <SectionHeader />
+                <DetailsCharacter />
+              </>
+            }
+          />
+        )}
       </Routes>
     </Router>
   );
